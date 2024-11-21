672370a53a998075c623c602efdb906367afa328
yokxim <yooksi53@gmail.com>
2024-11-21 09:28:52 +0900

    Revert "feat: AOP를 활용해 매니저 등록 시 오류와 상관없이 로그를 남기도록 설정" revert kotlin refactoring
    This reverts commit 107a2b2d986f640250d07e6c10f4da0ede08e6e9.

--- src/main/java/org/example/expert/aop/log/LoginLogRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.expert.aop.log;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginLogRepository extends JpaRepository<LoginLog, Long> {
-}
